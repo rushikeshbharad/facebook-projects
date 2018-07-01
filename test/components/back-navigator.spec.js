64b48da9317a3a588fb9db3e75618ae8d7b72d0e
rushikeshbharad <rushicbharad@gmail.com>
2018-07-01 15:21:54 +0530

    Added first enzyme + mocha React component unit test file

--- test/components/back-navigator.spec.js
+++ test/components/back-navigator.spec.js
@@ -1,14 +1,37 @@
-// // library imports
-// import React from 'react';
-// import { expect } from 'chai';
-// import { mount } from 'enzyme';
-//
-// // import component
-// import BackNavigator from '../src/components/back-navigator';
-//
-// describe('#<BackNavigator />', () => {
-// 	it('renders with text "<"', () => {
-// 		const wrapper = mount(<BackNavigator />);
-// 		expect(wrapper.text()).to.eq('<');
-// 	});
-// });
+// configure enzyme
+import '../setupEnzyme';
+
+// library imports
+import React from 'react';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+
+// import component
+import BackNavigator from '../../src/components/back-navigator';
+
+describe('#<BackNavigator />', () => {
+	it('should renders with text "<"', () => {
+		const wrapper = shallow(<BackNavigator />);
+		expect(wrapper.text()).to.eq('<');
+	});
+
+	it('should renders with class "back-navigator"', () => {
+		const wrapper = shallow(<BackNavigator />);
+		expect(wrapper.hasClass('back-navigator')).to.be.true;
+	});
+
+	it('should renders with provided className', () => {
+		const wrapper = shallow(<BackNavigator classNames="new-class" />);
+		expect(wrapper.hasClass('new-class')).to.be.true;
+	});
+
+	it('should call onClick prop on clicking the navigator', () => {
+		let clicked = false;
+		const onClick = () => {
+			clicked = true;
+		};
+		const wrapper = shallow(<BackNavigator onClick={onClick} />);
+		wrapper.simulate('click');
+		expect(clicked).to.be.true;
+	});
+});
